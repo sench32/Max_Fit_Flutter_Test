949e42986caae79469cfbc99377d583072a0d909
Sergei Sorokin <sergeisorokin@MacBook-Pro-Bender-3.local>
2020-04-04 23:29:31 +0300

    2-e Video 8.41 time

--- maxfit/lib/main.dart
+++ maxfit/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart'; //https://www.youtube.com/watch?v=yS6TRvCmCxg&feature=youtu.be
-import 'domain/workout.dart';
+import 'package:maxfit/screens/auth.dart';
+
+
 
 void main() => runApp(MaxFitApp());
 
@@ -13,103 +15,7 @@ class MaxFitApp extends StatelessWidget{
         primaryColor: Color.fromRGBO(50, 65, 85, 1),
         textTheme: TextTheme(title: TextStyle(color: Colors.white))
       ),
-      home: HomePage(),
+      home: AuthorizationPage(),
     );
   }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Scaffold(
-        backgroundColor: Theme.of(context).primaryColor,
-        appBar: AppBar(title: Text('MaxFit'), leading: Icon(Icons.fitness_center),),
-        body: WorkoutsList(),
-      ),
-    );
-  }
-}
-
-class WorkoutsList  extends StatelessWidget {
-  
-  final workouts = <Workout>[
-    Workout(title: 'Test 1', auhtor: 'Max 1', description: 'Test Workout 1', level: 'Beginner'),
-    Workout(title: 'Test 2', auhtor: 'Max 2', description: 'Test Workout 2', level: 'Intermediate'),
-    Workout(title: 'Test 3', auhtor: 'Max 3', description: 'Test Workout 3', level: 'Advancet'),
-    Workout(title: 'Test 4', auhtor: 'Max 4', description: 'Test Workout 4', level: 'Beginner'),
-    Workout(title: 'Test 5', auhtor: 'Max 5', description: 'Test Workout 5', level: 'Intermediate'),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Container(
-        child: ListView.builder(
-          itemCount: workouts.length,
-          itemBuilder: (context, i){
-            return Card(
-              elevation: 2.0,
-              margin: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-              child: Container(
-                decoration: BoxDecoration(color:  Color.fromRGBO(50, 65, 85, 0.9 ) ),
-                child:ListTile(
-                  contentPadding: EdgeInsets.symmetric(horizontal: 10),
-                  leading: Container(
-                    padding: EdgeInsets.only(right: 12),
-                    child: Icon(Icons.fitness_center, color: Theme.of(context).textTheme.title.color),
-                    decoration: BoxDecoration(
-                      border: Border(right: BorderSide(width: 1, color: Colors.white24))
-                    ),
-                    ),
-                  title: Text(workouts[i].title, style: TextStyle(color: Theme.of(context).textTheme.title.color, fontWeight: FontWeight.bold),),
-                  trailing: Icon(Icons.keyboard_arrow_right, color:Theme.of(context).textTheme.title.color),
-                  subtitle: subtitle(context, workouts[i]),
-                ),
-                ),
-            );
-          }
-        ),
-      ),
-    );
-  }
-}
-
-Widget subtitle(BuildContext context, Workout workout){
-  var color = Colors.grey;
-  double indicatorLevel = 0;
-
-  switch (workout.level){
-    case 'Beginner':
-      color = Colors.green;
-      indicatorLevel = 0.33;
-      break;
-    case 'Intermediate':
-      color = Colors.yellow;
-      indicatorLevel = 0.66;
-      break;
-    case 'Advancet':
-      color = Colors.red;
-      indicatorLevel = 1;
-      break;
-  }
-
-  return Row(
-    children: <Widget>[
-      Expanded(
-        flex: 1,
-        child: LinearProgressIndicator(
-          backgroundColor: Theme.of(context).textTheme.title.color,
-          value: indicatorLevel,
-          valueColor: AlwaysStoppedAnimation(color)
-        ),
-      ),
-      SizedBox(width: 10),
-      Expanded(flex: 3,
-      child: Text(workout.level, style: TextStyle(color:Theme.of(context).textTheme.title.color ),)
-      )
-    ],
-  );
 }
---
+++ maxfit/lib/screens/auth.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class AuthorizationPage extends StatefulWidget {
+  AuthorizationPage({Key key}) : super(key: key);
+
+  @override
+  _AuthorizationPageState createState() => _AuthorizationPageState();
+}
+
+class _AuthorizationPageState extends State<AuthorizationPage> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    Widget _logo(){
+      return Padding(
+        padding: EdgeInsets.only(top: 100),
+        child: Container(
+          child: Align(
+            child: Text('MAXFIT', style: TextStyle(fontSize: 45, fontWeight: FontWeight.bold, color: Colors.white))
+          )
+        )
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: Column(
+        children: <Widget>[
+          _logo(),
+          //_form(),
+        ],
+      ),
+    );
+  }
+}
