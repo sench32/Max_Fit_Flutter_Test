52acf7486e4df9efa4d46868addeef15568dbf43
Sergei Sorokin <sergeisorokin@MacBook-Pro-Bender-3.local>
2020-04-05 02:49:25 +0300

    2-e Vidio full

--- maxfit/lib/main.dart
+++ maxfit/lib/main.dart
@@ -1,7 +1,8 @@
-import 'package:flutter/material.dart'; //https://www.youtube.com/watch?v=yS6TRvCmCxg&feature=youtu.be
-import 'package:maxfit/screens/auth.dart';//https://www.youtube.com/watch?v=fjdwm88GeWo&list=PLu_62Q68DvTpWKx289HnZq7_19e84vSpr&index=2
+//https://www.youtube.com/watch?v=yS6TRvCmCxg&feature=youtu.be
+//https://www.youtube.com/watch?v=fjdwm88GeWo&list=PLu_62Q68DvTpWKx289HnZq7_19e84vSpr&index=2
 
-
+import 'package:flutter/material.dart';
+import 'package:maxfit/screens/auth.dart';
 
 void main() => runApp(MaxFitApp());
 
